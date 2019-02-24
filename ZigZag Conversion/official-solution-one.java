2e20af8ea84316470081176566978f039d2a360c
xyt111 <37143780+xyt111@users.noreply.github.com>
2019-02-24 13:56:51 +0800

    add time complexity

--- ZigZag Conversion/official-solution-one.java
+++ ZigZag Conversion/official-solution-one.java
@@ -21,8 +21,9 @@ class Solution {
         return ret.toString();
     }
 }
-//
-6: 构建一个stringBuilder的arraylist，stringBuilder也是字符串，用法参考https://www.cnblogs.com/Lxiaojiang/p/6442137.html
+/*6: 构建一个stringBuilder的arraylist，stringBuilder也是字符串，用法参考https://www.cnblogs.com/Lxiaojiang/p/6442137.html
 7: 第一次循环，循环次数为zigzag行数与字符串长度的最小值，n次（n = s.length），构建第六行创建的arraylist
 13: 第二次循环，遍历字符串的每个character，循环次数为n次（n = s.length），第二次循环其实就是对字符串的zigzag形式从第一个遍历到最后一个
 19-21: 遍历第六行创建的arraylist，将每一行的字符串加在一起，打印出来
+总的复杂度为O（n）
+*/
