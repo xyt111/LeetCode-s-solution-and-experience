0e18510d972712696231c0a88d9329c2850eeba9
xyt111 <37143780+xyt111@users.noreply.github.com>
2019-02-25 22:44:26 +0800

    append comments

--- ZigZag Conversion/official-solution-two.java
+++ ZigZag Conversion/official-solution-two.java
@@ -17,3 +17,12 @@ class Solution {
         return ret.toString();
     }
 }
+/*
+6: construct string ret by StringBuilder
+7: n indicates(refers to) string ret's length
+10: traverse all the line of zigzag pattern of string ret(line by line)
+11 and 12: get the character in a straight line and append it to string ret 
+13: get the character between two character in straight line and append it to string ret
+time complexity is O(n), n indicates the length of string ret
+space complexity is O(n), n refers to the length of string ret
+*/
